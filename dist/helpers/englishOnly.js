5606a31da6219c14c5a8f4a3777bbb893b2f3356
Soft <44058937+softchun@users.noreply.github.com>
2023-02-24 00:34:17 +0700

    Delete dist directory

--- dist/helpers/englishOnly.js
+++
@@ -1,9 +0,0 @@
-/**
- * Test if the string is only english letters and symbols.
- * @param str {string} The string to test.
- * @return {boolean} true if the string is only english letters and symbols. Otherwise, false.
- */
-function englishOnly(str) {
-    return /^[~`!@#$%^&*()_+=[\]\{}|;':",.\/<>?a-zA-Z0-9-]+$/.test(str);
-}
-module.exports = englishOnly;
